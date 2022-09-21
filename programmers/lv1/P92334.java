e36bddf3770d8b054931bfcb80f308c2f1fc58bb
rvlwldev <rvlwlasd77@gmail.com>
2022-09-21 19:02:04 +0900

    신고결과받기

--- programmers/lv1/P92334.java
+++ programmers/lv1/P92334.java
@@ -9,7 +9,6 @@ import java.util.Set;
 class Solution {
 
     public int[] solution(String[] id_list, String[] reportDup, int k) {
-        
         int[] answer = new int[id_list.length];
         Arrays.fill(answer, 0);
 
