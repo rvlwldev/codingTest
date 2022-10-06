94f6b17d5f5e4ebbd4054b8993afec3cc3a89970
rvlwldev <rvlwlasd77@gmail.com>
2022-10-06 20:50:20 +0900

    아이템 줍기

--- programmers/lv3/P87694.java
+++ programmers/lv3/P87694.java
@@ -9,40 +9,44 @@ class Solution87694 {
         int answer = 0;
 
         // Y, X 순서
-        int[][] graph = new int[101][101];
+        // 최대 50이라서 102 까지
+        // 인덱스는 0부터 시작해서 + 1
+        // 다음 길을 찾을때 에러 때문에 + 1
+        int[][] graph = new int[102][102];
 
         // 1. 2차원 배열로 맵을 만든다
         // 2. 주어진 사각형들보다 -1 작은 맵으로 0 을 채워서 테두리만 남긴다.
         // 3. 위는 2배 크게 만든다
-
         graph = createGraph(rectangle, graph);
 
         graph[itemY * 2][itemX * 2] = 7;
         graph[characterY * 2][characterX * 2] = 3;
 
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] { characterY * 2, characterX * 2, 0 });
+        q.offer(new int[] { characterY * 2, characterX * 2, 0 }); // 현재 캐릭터 위치
 
         int[] yList = new int[] { 1, -1, 0, 0 };
         int[] xList = new int[] { 0, 0, 1, -1 };
 
+        // DFS
         while (!q.isEmpty()) {
 
             int[] info = q.poll();
 
-            int y = info[0];
+            int y = info[0]; 
             int x = info[1];
-            int c = info[2];
+            int c = info[2]; // 현재 진행한 길 카운트
 
             for (int i = 0; i < yList.length; i++) {
 
+                // 아이템 주으면
                 if (graph[y + yList[i]][x + xList[i]] == 7) {
-                    return (c + 1) / 2;
+                    return (c + 1) / 2; // 아이템으로 위치 이동후 / 2
                 }
 
                 if (graph[y + yList[i]][x + xList[i]] == 1) {
-                    graph[y + yList[i]][x + xList[i]] = 0;
-                    q.offer(new int[] { y + yList[i], x + xList[i], c + 1 });
+                    graph[y + yList[i]][x + xList[i]] = 0; // 이미 지나온길은 다시 갈수 없음
+                    q.offer(new int[] { y + yList[i], x + xList[i], c + 1 }); 
                 }
             }
 
